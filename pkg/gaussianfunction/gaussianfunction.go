145aac0199a3a53d605f651d37a42f3400da61e2
senyc <95313103+senyc@users.noreply.github.com>
2023-10-14 11:54:35 -0400

    feat: update guassian function for variable start

--- pkg/gaussianfunction/gaussianfunction.go
+++ pkg/gaussianfunction/gaussianfunction.go
@@ -2,4 +2,5 @@ package gaussianfunction
 
 //"Sum of the First n Natural Numbers" formula or "Gauss's formula." 
 func GuassianFunction(s []int) int {
-	return (len(s) * (len(s) + 1)) / 2}
+	return int(len(s) * (2 * s[0] + (len(s) -1)) / 2)
+}
--- pkg/gaussianfunction/gaussianfunction_test.go
+++ pkg/gaussianfunction/gaussianfunction_test.go
@@ -14,6 +14,7 @@ func TestSum(t *testing.T) {
 		{"Ends at 1", []int{1}, 1},
 		{"Ends at 2", []int{1, 2}, 3},
 		{"Ends at 5", []int{1, 2, 3, 4, 5}, 15},
+		{"Starts at 6", []int{6, 7, 8, 9, 10}, 40},
 	}
 
 	for _, v := range test {
